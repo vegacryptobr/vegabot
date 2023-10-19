ad04acb2ace4ebf5a1c1221bb4092a0a3585f2a2
vegacryptobr <contato@vegacrypto.xyz>
2023-10-18 22:51:10 -0300

    commit

--- client/pages/chat.tsx
+++ client/pages/chat.tsx
@@ -163,7 +163,7 @@ export default function Chat() {
         try {
             setIsLoading(true)
             setTipsView('flex hidden')
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://chatvegacrypto.rj.r.appspot.com/predict', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(requestBody),
